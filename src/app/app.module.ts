f709bedc7efc9a1ad7116fcd566c9ecb9c236f7f
Fabiano Amorim <fabiano_amorim@hotmail.com>
2022-09-09 08:46:48 -0300

    configurar-rotas-fabiano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { HomeComponent } from './home/home.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    HomeComponent,
-    CadastroComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+//Angular route guards are interfaces provided by Angular which, when implemented, allow us to control 
+//the accessibility of a route based on conditions provided in class implementation of that interface.
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+import { AuthService } from './../auth/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) : Observable<boolean> | boolean {
+
+    console.log('AuthGuard');
+
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso(){
+    if (this.authService.userIsAuth()){
+      return true;
+    } 
+
+    this.router.navigate(['/login']);
+    return false;
+  }
+  
+}
