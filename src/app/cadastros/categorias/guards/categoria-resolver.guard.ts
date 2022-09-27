453794cab29a88561045075d9be5ffcf4d830379
xexel10 <105397253+xexel10@users.noreply.github.com>
2022-09-27 13:45:09 -0300

    teste

--- src/app/cadastros/categorias/guards/categoria-resolver.guard.ts
+++ src/app/cadastros/categorias/guards/categoria-resolver.guard.ts
@@ -14,17 +14,17 @@ export class CategoriaResolverGuard implements Resolve<Categoria> {
   constructor(private service: CategoriaService){}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Categoria | Observable<Categoria> {
-    
+
     if (route.params && route.params['id']){
       return this.service.loadByID(route.params['id'])
     }
 
     return of({
-      id: number | 0,
-      nome: string | '',
-      status: boolean
+      id: number | null,
+      nome: string | null
+      status: boolean | null
     });
 
   }
-  
+
 }
