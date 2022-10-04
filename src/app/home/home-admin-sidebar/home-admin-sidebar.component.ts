00395d81cd365943dc7f3cc42911b44b150ca4fd
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-10-04 16:05:16 -0300

    menu lateral

--- src/app/home/home-admin-sidebar/home-admin-sidebar.component.ts
+++ src/app/home/home-admin-sidebar/home-admin-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-admin-sidebar',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAdminSidebarComponent implements OnInit {
 
+  @ViewChild('sideBar') sideBar!:  ElementRef;
+  @ViewChild('iconeMenu') iconeMenu!:  ElementRef;
+  mostrarMenu = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("onineit ",this.sideBar);
   }
+
+  ngAfterInit(){
+    console.log(this.sideBar);
+
+  }
+  mudarNav(){
+    let el = this.sideBar.nativeElement;
+
+    if  (el.style.width == '250px') {
+      el.setAttribute('style', 'width: 0px');
+      this.mostrarMenu = true;
+    }
+    else {
+      el.setAttribute('style', 'width: 250px');
+      this.mostrarMenu = false;
+    }
+
+  }
+
 
 }
