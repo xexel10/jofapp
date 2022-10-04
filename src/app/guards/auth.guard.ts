45b560a5199822a89228def53eba259abaa117c5
xexel10 <105397253+xexel10@users.noreply.github.com>
2022-10-04 10:44:47 -0300

    Format Guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -19,7 +19,6 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ) : Observable<boolean> | boolean {
-
     return this.verificarAcesso();
   }
 
