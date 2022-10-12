87552191436e142c02c317673c56eed308e4d764
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-10-11 23:16:33 -0300

    tokenService

--- src/app/auth/token/token.service.ts
+++ src/app/auth/token/token.service.ts
@@ -9,47 +9,63 @@ const KEY = 'ACCESS TOKEN';
 })
 export class TokenService {
 
-
+  horaToken: any;
+  horaAtual: any;
+  tempoValidadeToken: any;
   constructor(private http: HttpClient) { }
 
-  hasToken(){
+  hasToken() {
     //      truque para retornar booleano  
-
-    //console.log('Token expirado')
     return !!this.getToken()
 
-}
-
-setToken(token: any, refresh?: any){
-    window.localStorage.setItem(KEY, token);
-    if (refresh != undefined)
-      window.localStorage.setItem('REFRESH', refresh )
-    window.localStorage.setItem('HORA', String(new Date().getTime()))
-
-}
-getToken(){
-  if (window.localStorage.getItem(KEY)) {
-    var horaToken = window.localStorage.getItem('HORA');
-    var horaAtual = new Date().getTime();
-    if (horaAtual > Number(horaToken)+60000){
-        console.log('token venceu');
-        //refresh do token
-        var record = {refresh: window.localStorage.getItem('REFRESH')}
-        this.http.post(environment.TOKEN_REFRESH_URL, record).subscribe({
-          next: (token) => {this.setToken(token['access']); 
-                      console.log("TOKEN ATUALIZADO")},
-          error: (erro) => {console.log("deu pau no login")}
-        })
-      } else {
-      console.log('Token existe e é valido');
-    }
   }
-  return window.localStorage.getItem(KEY)
 
-}
 
-removeToken(){
+  setToken(token: any, refresh?: any) {
+    window.localStorage.setItem(KEY, token);
+    window.localStorage.setItem('HORA', String(new Date().getTime()));
+
+    if (refresh != undefined)
+      window.localStorage.setItem('REFRESH', refresh)
+
+  }
+
+  getToken() {
+    this.verificaTokenExpirado();
+
+    return window.localStorage.getItem(KEY)
+
+  }
+
+  removeToken() {
     window.localStorage.removeItem(KEY);
 
-}
+  }
+
+  verificaTokenExpirado() {
+      this.horaToken = window.localStorage.getItem('HORA');
+      this.horaAtual = new Date().getTime();
+      if (this.horaAtual > Number(this.horaToken) + 60000) {
+        console.log('token vencido, renovar');
+        this.renovarToken();
+      }
+    else {
+      // se não tiver tokenw
+    }
+  }
+
+
+  renovarToken() {
+    var record = { refresh: window.localStorage.getItem('REFRESH') };
+    this.http.post(environment.TOKEN_REFRESH_URL, record).subscribe({
+      next: (token) => {
+        this.setToken(token['access']);
+        console.log("TOKEN ATUALIZADO")
+      },
+      error: (erro) => { console.log("deu pau ao recuperar token");
+        this.removeToken();
+      }
+    })
+  }
+
 }
