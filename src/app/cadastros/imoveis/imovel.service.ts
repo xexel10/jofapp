8789f1a74da91e22cc4501ed52056a177fbcdfd2
Fabiano Amorim <fabiano_amorim@hotmail.com>
2022-10-12 06:46:36 -0300

    Upload Files Continue

--- src/app/cadastros/imoveis/imovel.service.ts
+++ src/app/cadastros/imoveis/imovel.service.ts
@@ -15,4 +15,18 @@ export class ImovelService extends CrudService<Imovel> {
     super(http, `${environment.API}imoveis/`);
   }
 
+  upload(files: Set<File>, url: string) {
+
+    const formData = new FormData();
+    files.forEach(file => formData.append('file', file, file.name));
+
+    // const request = new HttpRequest('POST', url, formData);
+    // return this.http.request(request);
+
+    return this.http.post(url, formData, {
+      observe: 'events',
+      reportProgress: true
+    });
+  }
+
 }
