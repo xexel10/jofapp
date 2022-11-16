6a172c71a8f97a7c873cf2413a959271b87ae65a
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-11-16 15:35:57 -0300

    variavel ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   //TOKEN_URL: 'http://ottoni.vps-kinghost.net:8000/token/',
-  TOKEN_URL: 'http://127.0.0.1:8000/token/',
+  TOKEN_URL: 'http://http://ottoni.vps-kinghost.net:8000/token/',
   //TOKEN_REFRESH_URL: 'http://ottoni.vps-kinghost.net:8000/tokenRefresh/',
-  TOKEN_REFRESH_URL: 'http://127.0.0.1:8000/tokenRefresh/',
+  TOKEN_REFRESH_URL: 'http://ottoni.vps-kinghost.net:8000/tokenRefresh/',
 
   //API: 'http://ottoni.vps-kinghost.net:8000/api/',
-  API: 'http://127.0.0.1:8000/api/',
+  API: 'http://ottoni.vps-kinghost.net:8000/api/',
 
   BASE_URL: '/api'
 };
