c0839b027834c6aaca40e5e4ef8a7df2e74e0134
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-11-17 09:25:19 -0300

    correcao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //TOKEN_URL: 'http://ottoni.vps-kinghost.net:8000/token/',
-  TOKEN_URL: 'http://http://ottoni.vps-kinghost.net:8000/token/',
+  TOKEN_URL: 'http://ottoni.vps-kinghost.net:8000/token/',
   //TOKEN_REFRESH_URL: 'http://ottoni.vps-kinghost.net:8000/tokenRefresh/',
   TOKEN_REFRESH_URL: 'http://ottoni.vps-kinghost.net:8000/tokenRefresh/',
 
